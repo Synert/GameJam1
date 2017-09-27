739b3c9dd065a17f7bbe669149250c719f4cc78f
StankyDan <danthemanwow@hotmail.com>
2017-09-27 16:53:52 +0100

    Truck spawner added

---
+++ Trucks/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public float spawnFrequency = 5.0f;
+    public float spawnStart = 1.0f;
+    public GameObject[] Trucks;
+    public Vector3 truckPosition;
+
+
+	void Start ()
+    {
+        truckPosition = transform.position;
+        InvokeRepeating("Spawn", spawnStart, spawnFrequency);
+    }
+	
+    void Spawn()
+    {
+        int truckArray = Random.Range(0, Trucks.Length);
+        Instantiate(Trucks[truckArray], truckPosition, Quaternion.identity);
+    }
+
+
+
+    //GameObject.FindGameObjectsWithTag("Background");
+    //Need to change Z axis of spawning bkacground trucks to 1.
+
+    void Update ()
+    {
+       
+    }
+}
