ba97f5fd75493d31533ea35f54f8fa70b72ed070
josh <iownall99@live.co.uk>
2017-09-27 14:15:38 +0100

    Added base item spawning

---
+++ Trucks/Assets/Data.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour {
+
+	public itemData objData = new itemData();
+}
+
+public struct itemData {
+
+	public float cost;
+
+}
---
+++ Trucks/Assets/simpleMoving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleMoving : MonoBehaviour {
+
+	public GameObject obj = null;
+	public float speed = 1;
+	public float points = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().AddForce (Vector2.right * speed);
+		points += GetComponent<Rigidbody2D> ().velocity.x;
+		if (Input.GetKeyDown (KeyCode.E)) {
+			if (obj != null) {
+				if (points >= obj.GetComponent<Data> ().objData.cost) {
+					points -= obj.GetComponent<Data> ().objData.cost;
+					GameObject temp = GameObject.Instantiate (obj, transform.position, obj.transform.rotation);
+					temp.transform.position = temp.transform.position - new Vector3 (0, 0, 2);
+					temp.transform.position += new Vector3 (GetComponent<Rigidbody2D> ().velocity.x, 0, 0);
+				}
+			}
+		}
+	}
+}
