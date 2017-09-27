0948e889031fef5b4739434d87c380f1302237ed
jplant20 <Josephsp@hotmail.co.uk>
2017-09-27 17:18:25 +0100

    Made a basic level editor to spawn the trucks and set the player start position in level editor demo. Also made a script to change the scene when player collides with an invisible box at the end to change the level (currently defaults to 'trucks').

---
+++ Trucks/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D level;
+    public ColourToPrefab[] ColourMappings;
+
+	void Start ()
+    {
+        LevelGeneration();
+	}
+	
+    void LevelGeneration ()
+    {
+        for (int x = 0; x < level.width; x++)
+        {
+            for (int y = 0; y < level.height; y++)
+            {
+                TileGeneration(x, y);
+            }
+        }
+    }
+
+    void TileGeneration (int x, int y)
+    {
+        Color PixelColour = level.GetPixel(x, y);
+        if (PixelColour.a == 0)
+        {
+            return;
+        }
+
+        foreach (ColourToPrefab colorMapping in ColourMappings)
+        {
+            if (colorMapping.colour.Equals(PixelColour))
+            {
+                Vector2 position = new Vector2(x, y);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
---
+++ Trucks/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] private string loadlevel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(loadlevel);
+        }
+    }
+}
