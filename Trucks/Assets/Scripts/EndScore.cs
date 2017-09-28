0293ea47fce3c405d78e91c550632428ab4e1f59
Connor Rhone <Connor2.Rhone@live.uwe.ac.uk>
2017-09-28 16:23:52 +0100

    Added end goal and score

---
+++ Trucks/Assets/Scripts/EndScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScore : MonoBehaviour
+{
+    public ScoreTimer scoreTimer;
+
+    Text score;
+
+    void Start ()
+    {
+        score.text = "Your Final Score is: " + scoreTimer.scoreTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Trucks/Assets/Scripts/ScoreTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTimer : MonoBehaviour {
+
+    public float scoreTime = 10000f;
+    Text score;
+	
+	void Start ()
+    {
+        score = GetComponent<Text> ();
+    }
+	
+	
+	void Update ()
+    {
+
+        scoreTime -= ((Time.unscaledDeltaTime) *  50);
+        score.text = "Score: " + (int)scoreTime;
+	}
+}
